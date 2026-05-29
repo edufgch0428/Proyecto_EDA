b34fe6606f609325ad7e1a23f74dc9817ae81d0c
alaydv <Usuario@LAPTOP-59E426VD>
2026-05-29 00:05:50 -0500

    Se agrego comentarios de forma general

--- src/main/java/com/mycompany/proyecto_eda/Menu.java
+++ src/main/java/com/mycompany/proyecto_eda/Menu.java
@@ -12,11 +12,11 @@ public class Menu {
     public void mostrarMenu(){
         System.out.println("========== MENU ==========");
         System.out.println("1. Quick sort de Playtime");
-        System.out.println("2. (Merge sort de Metacritic");
+        System.out.println("2. Merge sort de Metacritic");
         System.out.println("3. Colision - Sondeo Cuadratico");
         System.out.println("4. Top 10 juegos mas populares por cantidad de duenios.");
         System.out.println("5. Top 20 juegos con mayor PlayTime");
-        System.out.println("6. Top 20 juegos con mayor PlayTime");
+        System.out.println("6. Top 20 juegos con mayor Metacritic");
         System.out.println("7. Salir");
         System.out.print("Ingrese una opcion: ");
     }

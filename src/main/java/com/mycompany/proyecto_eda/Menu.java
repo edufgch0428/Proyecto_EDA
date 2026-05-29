d3897e39a211430a10edb4139a220a166989af06
alaydv <Usuario@LAPTOP-59E426VD>
2026-05-28 23:29:46 -0500

    Cambios al menu 1.0

--- src/main/java/com/mycompany/proyecto_eda/Menu.java
+++ src/main/java/com/mycompany/proyecto_eda/Menu.java
@@ -12,7 +12,7 @@ public class Menu {
     public void mostrarMenu(){
         System.out.println("========== MENU ==========");
         System.out.println("1. Quick sort de Playtime");
-        System.out.println("2. (NOMBRE DE COLISION) sort");
+        System.out.println("2. (Merge sort de Metacritic");
         System.out.println("3. Colision - Sondeo Cuadratico");
         System.out.println("4. Top 10 juegos mas populares por cantidad de duenios.");
         System.out.println("5. Top 20 juegos con mayor PlayTime");
